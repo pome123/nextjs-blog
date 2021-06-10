a4485e4b61da3566e0bdcace61537ff978c038e2
fujine <fujine@sria.co.jp>
2021-06-10 17:32:06 +0900

    マークダウンファイルをHTMLファイルに変換。

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,22 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <div>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </div>
     </Layout>
   )
 }
@@ -26,7 +34,7 @@ export async function getStaticPaths() {
 
 // idに基づいて必要なデータを取得
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
